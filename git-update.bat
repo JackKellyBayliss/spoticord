298a1568b94f698f96b32ed4d4bfddb96249fd24
JackKellyBayliss <27035595+JackKellyBayliss@users.noreply.github.com>
2018-01-27 19:50:44 +0000

    Confirmation Git Update Message

--- git-update.bat
+++ git-update.bat
@@ -1,6 +1,11 @@
 @echo off
-color 7F
+setlocal
+:PROMPT
+SET /P REALLYSURE=Executing this git-update.bat file will override any changes you have made, are you sure you want to continue? (Y/[N])?
+IF /I "%REALLYSURE%" NEQ "Y" GOTO END
 git fetch --all
 git reset --hard origin/master
 git pull origin master
+:END
+endlocal
 pause
